576a61622f7f98aafa3dc4d0ef1f3f512cb3d582
Eli Fatsi <eli.fatsi@viget.com>
2013-06-05 11:56:56 -0600

    Made User class, added heavy rotation

--- app.rb
+++ app.rb
@@ -2,36 +2,15 @@ require 'bundler'
 Bundler.setup(:default)
 Bundler.require
 
+load 'user.rb'
+
 get '/' do
   erb :home
 end
 
 get '/user' do
-  username = params[:username]
+  @user = User.new(params[:username])
+  @song = @user.last_song
 
-  @song     = get_last_song_from(username)
-  @user_url = url_from(username)
   erb :user
-end
-
-def get_last_song_from(username)
-  song = user_data(username)['lastSongPlayed']
-  {
-    :icon   => song['icon'],
-    :url    => song['url'],
-    :artist => song['albumArtist'],
-    :title  => song['name']
-  }
-end
-
-def url_from(username)
-  "http://rdio.com#{user_data(username)[:url]}"
-end
-
-def user_data(username)
-  @user_data ||= client.findUser(:vanityName => username, :extras => 'lastSongPlayed')
-end
-
-def client
-  @client ||= RdioApi.new(:consumer_key => 'n89y2yxfrdek38tsv6st72sg', :consumer_secret => 'WzP6z4Qf9u')
 end
---
+++ user.rb
@@ -0,0 +1,40 @@
+class User
+
+  def initialize(username)
+    @username = username
+  end
+
+  def last_song
+    song = user_data['lastSongPlayed']
+    {
+      :icon   => song['icon'],
+      :url    => song['url'],
+      :artist => song['albumArtist'],
+      :title  => song['name']
+    }
+  end
+
+  def heavy_rotation
+    key = user_data[:heavyRotationKey]
+    hr  = client.get(:keys => key)
+    hr[key][:tracks].map{|a| "#{a[:artist]} - #{a[:album]}"}.uniq
+  end
+
+  def url
+    "http://rdio.com#{account_url}"
+  end
+
+  private
+
+  def client
+    @client ||= RdioApi.new(:consumer_key => 'n89y2yxfrdek38tsv6st72sg', :consumer_secret => 'WzP6z4Qf9u')
+  end
+
+  def user_data
+    @user_data ||= client.findUser(:vanityName => @username, :extras => 'lastSongPlayed, heavyRotationKey')
+  end
+
+  def account_url
+    user_data[:url]
+  end
+end
